8c2b59955e29e8d945c2b1d03eaa8cac2ea48353
Nurgasyr <47244885+Nurgasyr@users.noreply.github.com>
2019-02-01 17:57:36 +0600

    3

--- Program.cs
+++ Program.cs
@@ -4,31 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
-    class Student
-    {
-        string Name {get;set;}
-        int id { get; set; }
-        int Year { get; set; }
-        public Student(string name,int id)
-        {
-            Name = name;id = 18;
-            Year = 2019;
-        }
-        public void NewYear()
-        {
-            Console.WriteLine("Student:{0};id:{1};Year:{2}", Name, id,Year++);
-        }
-    }
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Student s1 = new
-                Student("Nurgasyr Tleuov", 1);
-            s1.NewYear();
+            int n,a;
+            n = Convert.ToInt32(Console.ReadLine());                         //input sum of numbers
+            List<int> array = new List<int>();                               //creat an array 
+            for(int i = 0; i < n; i++)
+            {
+                a = Convert.ToInt32(Console.ReadLine());                     //input numbers
+                array.Add(a);                                                //add in the array
+            }
+            for(int i = 0; i < n; i++)                                       
+            {
+                for (int j = 0; j < 2; j++)                                 // to double numbers 
+                {
+                    Console.Write(array[i]+" ");                             //output the numbers
+                }
+            }
         }
     }
 }
