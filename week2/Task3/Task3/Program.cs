dcd18658800ce2288809e0deb8243c6c9e660a4e
Nurgasyr <47244885+Nurgasyr@users.noreply.github.com>
2019-02-10 23:49:46 +0600

    Add files via upload

---
+++ week2/Task3/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\NUR\source\repos\Task3");                                           //directory
+            PrintInfo(dir, 0);                                                                                                  //get all files and directories
+        }
+
+        static void PrintInfo(FileSystemInfo fs, int k)
+        {
+            string s = new string(' ', k);                                                                                      //k space 
+            Console.WriteLine(s + fs.Name);
+
+            if (fs.GetType() == typeof(DirectoryInfo))
+            {
+                FileSystemInfo[] fss = ((DirectoryInfo)fs).GetFileSystemInfos();
+                for (int i = 0; i < fss.Length; ++i)
+                {
+                    PrintInfo(fss[i], k + 3);                                                                                  //recursion
+                }
+            }
+        }
+    }
+}
